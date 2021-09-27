b44e77b22d72695290008606778397019f8e98a2
obiwac <obiwac@gmail.com>
2021-09-27 13:55:01 +0200

    Finally got scrolling to work in the aquaBSD console in the mouse device

--- aquabsd.alps.mouse/aquabsd_console_mouse.h
+++ aquabsd.alps.mouse/aquabsd_console_mouse.h
@@ -4,8 +4,6 @@
 #include <sys/fbio.h>
 #include <sys/consio.h>
 
-#define AQUABSD_SYSMOUSE_DEV "/dev/sysmouse"
-
 static inline int position_aquabsd_console_mouse(int x, int y) {
 	mouse_info_t mouse_info = { .operation = MOUSE_MOVEABS };
 
@@ -64,14 +62,13 @@ static inline int update_aquabsd_console_mouse(mouse_t* mouse) {
 		goto error;
 	}
 
-	mouse->axes[AXIS_X] =       (float) mouse_info.u.data.x / mode_info.vi_width;
-	mouse->axes[AXIS_Y] = 1.0 - (float) mouse_info.u.data.y / mode_info.vi_height;
-	mouse->axes[AXIS_Z] =       (float) mouse_info.u.data.z;
-
 	struct {
-		unsigned left   : 1;
-		unsigned middle : 1;
-		unsigned right  : 1;
+		unsigned left        : 1;
+		unsigned middle      : 1;
+		unsigned right       : 1;
+
+		unsigned scroll_down : 1;
+		unsigned scroll_up   : 1;
 	} button;
 
 	*(int*) &button = mouse_info.u.data.buttons;
@@ -79,6 +76,32 @@ static inline int update_aquabsd_console_mouse(mouse_t* mouse) {
 	mouse->buttons[BUTTON_LEFT  ] = button.left;
 	mouse->buttons[BUTTON_RIGHT ] = button.right;
 	mouse->buttons[BUTTON_MIDDLE] = button.middle;
+
+	mouse->axes[AXIS_X] =       (float) mouse_info.u.data.x / mode_info.vi_width;
+	mouse->axes[AXIS_Y] = 1.0 - (float) mouse_info.u.data.y / mode_info.vi_height;
+
+	mouse->axes[AXIS_Z] = 0;
+
+	if (button.scroll_down) mouse->axes[AXIS_Z] -= 1.0;
+	if (button.scroll_up  ) mouse->axes[AXIS_Z] += 1.0;
+
+	// "reset" mouse
+	// to be perfectly honest, I don't know why this works, but after a lot of trial and error, I've found it's the only way to get the two virtual "scrolling buttons" to actually register
+	// the relevant source files are (read them at risk of your own sanity) 'sys/dev/syscons/scmouse.c' & 'usr.sbin/moused/moused.c'
+	// I wish the people who wrote these monstrosities (especially 'scmouse.c') be sentenced to an eternel life of stubbing their toes against furniture
+	
+	mouse_info.operation = MOUSE_ACTION;
+	mouse_info.u.data.buttons |= 0b11000; // set the two virtual "scrolling buttons"
+
+	mouse_info.u.data.x = 0; // make sure we don't move around the mouse
+	mouse_info.u.data.y = 0;
+
+	if (ioctl(0, CONS_MOUSECTL, &mouse_info) < 0) {
+		goto error;
+	}
+
+	mouse_info.operation = MOUSE_GETINFO;
+	ioctl(0, CONS_MOUSECTL, &mouse_info); // unnecessary to check for errors here
 
 	return 0;
 
