6373d4287fc75f923cf575b44cb0137e01ea5f65
obiwac <obiwac@gmail.com>
2021-12-23 23:36:01 +0100

    wm: keyboard grabbed asynchonously when creating window again

--- aquabsd.alps.wm/functions.h
+++ aquabsd.alps.wm/functions.h
@@ -201,7 +201,7 @@ static int process_event(void* _wm, int type, xcb_generic_event_t* event) {
 		};
 
 		xcb_change_window_attributes(wm->root->connection, detail->window, XCB_CW_EVENT_MASK, attribs);
-		xcb_grab_button(wm->root->connection, 1, detail->window, XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE | XCB_EVENT_MASK_POINTER_MOTION | XCB_EVENT_MASK_BUTTON_MOTION, XCB_GRAB_MODE_SYNC, XCB_GRAB_MODE_SYNC, XCB_NONE, XCB_NONE, XCB_BUTTON_INDEX_ANY, XCB_MOD_MASK_ANY);
+		xcb_grab_button(wm->root->connection, 1, detail->window, XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE | XCB_EVENT_MASK_POINTER_MOTION | XCB_EVENT_MASK_BUTTON_MOTION, XCB_GRAB_MODE_SYNC, XCB_GRAB_MODE_ASYNC, XCB_NONE, XCB_NONE, XCB_BUTTON_INDEX_ANY, XCB_MOD_MASK_ANY);
 
 		win_t* win = add_win(wm, detail->window);
 		WIN_CONFIG
