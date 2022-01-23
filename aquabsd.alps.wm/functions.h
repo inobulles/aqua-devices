4db7c814b5693ec4c463609776212a39d18543c6
obiwac <obiwac@gmail.com>
2022-01-23 15:05:16 +0100

    wm: show that we support more atoms

--- aquabsd.alps.wm/functions.h
+++ aquabsd.alps.wm/functions.h
@@ -479,19 +479,6 @@ dynamic wm_t* create(void) {
 	xcb_ungrab_key(wm->root->connection, XCB_GRAB_ANY, wm->root->win, XCB_MOD_MASK_ANY);
 	xcb_grab_key(wm->root->connection, 1, wm->root->win, SUPER_MOD, XCB_GRAB_ANY, XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC);
 
-	// specify which atoms are supported in '_NET_SUPPORTED'
-
-	const xcb_atom_t supported_atoms[] = {
-		wm->root->ewmh._NET_SUPPORTED,
-		wm->root->ewmh._NET_CLIENT_LIST,
-
-		wm->root->ewmh._NET_WM_VISIBLE_NAME,
-		wm->root->ewmh._NET_WM_NAME,
-		wm->root->ewmh._NET_WM_STATE,
-	};
-
-	xcb_change_property(wm->root->connection, XCB_PROP_MODE_REPLACE, wm->root->win, wm->root->ewmh._NET_SUPPORTED, XCB_ATOM_ATOM, 32, sizeof(supported_atoms) / sizeof(*supported_atoms), supported_atoms);
-
 	// now, we move on to '_NET_SUPPORTING_WM_CHECK'
 	// this is a bit weird, but it's all specified by the EWMH spec: https://developer.gnome.org/wm-spec/
 
@@ -500,6 +487,22 @@ dynamic wm_t* create(void) {
 
 	xcb_change_property(wm->root->connection, XCB_PROP_MODE_REPLACE, wm->root->win, wm->root->ewmh._NET_SUPPORTING_WM_CHECK, XCB_ATOM_WINDOW, 32, 1, support_win_list);
 	xcb_change_property(wm->root->connection, XCB_PROP_MODE_REPLACE, wm->support_win, wm->root->ewmh._NET_SUPPORTING_WM_CHECK, XCB_ATOM_WINDOW, 32, 1, support_win_list);
+
+	// specify which atoms are supported in '_NET_SUPPORTED'
+
+	const xcb_atom_t supported_atoms[] = {
+		wm->root->ewmh._NET_SUPPORTED,
+		wm->root->ewmh._NET_SUPPORTING_WM_CHECK,
+		wm->root->ewmh._NET_CLIENT_LIST,
+
+		wm->root->ewmh._NET_WM_VISIBLE_NAME,
+		wm->root->ewmh._NET_WM_NAME,
+
+		wm->root->ewmh._NET_WM_STATE,
+		wm->root->ewmh._NET_WM_STATE_FULLSCREEN,
+	};
+
+	xcb_change_property(wm->root->connection, XCB_PROP_MODE_REPLACE, wm->root->win, wm->root->ewmh._NET_SUPPORTED, XCB_ATOM_ATOM, 32, sizeof(supported_atoms) / sizeof(*supported_atoms), supported_atoms);
 
 	// get all monitors and their geometries
 	// CRTC stands for "Cathode Ray Tube Controller", which is a dumb name, so AQUA calls them "providers"
