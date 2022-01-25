399ff2db9c0eab7445d529269de4a1f408c91c3a
obiwac <obiwac@gmail.com>
2022-01-25 11:44:34 +0100

    wm: regression where clicking with other modifiers would not pass events on to client

--- aquabsd.alps.wm/functions.h
+++ aquabsd.alps.wm/functions.h
@@ -117,7 +117,7 @@ static win_t* search_win(wm_t* wm, xcb_window_t id) {
 	}
 
 	if (!win) {
-		fprintf(stderr, "[aquabsd.alps.wm] WARN Window of id 0x%x was not found\n", id);
+		WARN("Window of id 0x%x was not found\n", id)
 	}
 
 	return NULL; // something's probably gonna crash ;)
@@ -403,7 +403,7 @@ static int process_event(void* _wm, int type, xcb_generic_event_t* event) {
 			// prevent event from passing through to the client
 
 			xcb_grab_pointer(wm->root->connection, 1, wm->root->win, XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE | XCB_EVENT_MASK_POINTER_MOTION | XCB_EVENT_MASK_BUTTON_MOTION, XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, XCB_NONE, XCB_NONE, XCB_CURRENT_TIME);
-			xcb_grab_button(wm->root->connection, 1, wm->root->win, XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE | XCB_EVENT_MASK_POINTER_MOTION | XCB_EVENT_MASK_BUTTON_MOTION, XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, XCB_NONE, XCB_NONE, XCB_BUTTON_INDEX_ANY, XCB_BUTTON_MASK_ANY);
+			xcb_grab_button(wm->root->connection, 1, wm->root->win, XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE | XCB_EVENT_MASK_POINTER_MOTION | XCB_EVENT_MASK_BUTTON_MOTION, XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, XCB_NONE, XCB_NONE, XCB_BUTTON_INDEX_ANY, SUPER_MOD);
 		}
 
 		else {
@@ -648,7 +648,7 @@ dynamic int set_name(wm_t* wm, const char* name) {
 
 dynamic int register_cb(wm_t* wm, cb_t type, uint64_t cb, uint64_t param) {
 	if (type >= CB_LEN) {
-		fprintf(stderr, "[aquabsd.alps.wm] Callback type %d doesn't exist\n", type);
+		WARN("Callback type %d doesn't exist\n", type)
 		return -1;
 	}
 
