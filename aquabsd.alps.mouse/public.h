36944e25a5ef32ad01fb032d4be748cb04f2c22a
obiwac <obiwac@gmail.com>
2022-07-10 22:11:47 +0200

    mouse: Function variables are marked as `extern` rather than `static`
    This is to silence GCC saying they're defined but unused for some reason.

--- aquabsd.alps.mouse/public.h
+++ aquabsd.alps.mouse/public.h
@@ -44,12 +44,12 @@ struct aquabsd_alps_mouse_t {
 	#define AQUABSD_ALPS_MOUSE_AQUABSD_CONSOLE_MOUSE
 #endif
 
-static unsigned (*aquabsd_alps_mouse_get_default_mouse_id) (void);
-static int (*aquabsd_alps_mouse_update_mouse) (unsigned mouse_id);
+extern unsigned (*aquabsd_alps_mouse_get_default_mouse_id) (void);
+extern int (*aquabsd_alps_mouse_update_mouse) (unsigned mouse_id);
 
-static unsigned (*aquabsd_alps_mouse_poll_button) (unsigned mouse_id, aquabsd_alps_mouse_button_t button);
-static float (*aquabsd_alps_mouse_poll_axis) (unsigned mouse_id, aquabsd_alps_mouse_axis_t axis);
+extern unsigned (*aquabsd_alps_mouse_poll_button) (unsigned mouse_id, aquabsd_alps_mouse_button_t button);
+extern float (*aquabsd_alps_mouse_poll_axis) (unsigned mouse_id, aquabsd_alps_mouse_axis_t axis);
 
-static aquabsd_alps_mouse_t* (*aquabsd_alps_mouse_register_mouse) (const char* name, aquabsd_alps_mouse_update_callback_t update_callback, void* update_cb_param, unsigned set_default);
+extern aquabsd_alps_mouse_t* (*aquabsd_alps_mouse_register_mouse) (const char* name, aquabsd_alps_mouse_update_callback_t update_callback, void* update_cb_param, unsigned set_default);
 
 #endif
