f503b1367710ba2b5c4e985b0835bf2d34406364
obiwac <obiwac@gmail.com>
2022-07-14 23:24:14 +0200

    ftime: Prevent AQUA apps from using device

--- aquabsd.alps.ftime/main.c
+++ aquabsd.alps.ftime/main.c
@@ -2,17 +2,14 @@
 #include <aquabsd.alps.ftime/functions.h>
 
 typedef enum {
-	CMD_CREATE = 0x6674, // 'ft'
+	TODO
 } cmd_t;
 
 uint64_t send(uint16_t _cmd, void* data) {
 	cmd_t cmd = _cmd;
 	uint64_t* args = data;
 
-	if (cmd == CMD_CREATE) {
-		double target = *(double*) &args[0];
-		return (uint64_t) create(target);
-	}
+	LOG_ERROR("This device is currently for exclusive use of other devices")
 
 	return -1;
 }
