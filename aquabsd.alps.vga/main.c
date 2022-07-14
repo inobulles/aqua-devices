ef51c4521746255b3fbe14a77bac0aee2ea6f138
obiwac <obiwac@gmail.com>
2022-07-14 16:39:45 +0200

    vga: Remove function arguments from `load`

--- aquabsd.alps.vga/main.c
+++ aquabsd.alps.vga/main.c
@@ -1,52 +1,44 @@
 #include <aquabsd.alps.vga/private.h>
 #include <aquabsd.alps.vga/functions.h>
 
-#define CMD_MODE_COUNT  0x6D63 // 'mc'
-#define CMD_MODES       0x6D64 // 'md'
+typedef enum {
+	CMD_MODE_COUNT  = 0x6D63, // 'mc'
+	CMD_MODES       = 0x6D64, // 'md'
 
-#define CMD_SET_MODE    0x736D // 'sm'
-#define CMD_FRAMEBUFFER 0x6662 // 'fb'
+	CMD_SET_MODE    = 0x736D, // 'sm'
+	CMD_FRAMEBUFFER = 0x6662, // 'fb'
 
-#define CMD_FLIP        0x666C // 'fl'
+	CMD_FLIP        = 0x666C, // 'fl'
 
-#define CMD_RESET       0x7672 // 'vr'
+	CMD_RESET       = 0x7672, // 'vr'
+} cmd_t;
 
-int load(
-	uint64_t (*_kos_query_device) (uint64_t, uint64_t name),
-	void* (*_kos_load_device_function) (uint64_t device, const char* name),
-	uint64_t (*_kos_callback) (uint64_t callback, int argument_count, ...)) {
+uint64_t send(uint16_t _cmd, void* data) {
+	cmd_t cmd = _cmd;
+	uint64_t* args = data;
 
-	kos_query_device = _kos_query_device;
-	kos_load_device_function = _kos_load_device_function;
-
-	return 0;
-}
-
-uint64_t send(uint16_t command, void* data) {
-	uint64_t* arguments = (uint64_t*) data;
-
-	if (command == CMD_MODE_COUNT) {
+	if (cmd == CMD_MODE_COUNT) {
 		return get_mode_count();
 	}
 
-	else if (command == CMD_MODES) {
+	else if (cmd == CMD_MODES) {
 		return (uint64_t) get_modes();
 	}
 
-	else if (command == CMD_SET_MODE) {
-		video_mode_t* mode = (video_mode_t*) arguments[0];
+	else if (cmd == CMD_SET_MODE) {
+		video_mode_t* mode = (void*) args[0];
 		return set_mode(mode);
 	}
 
-	else if (command == CMD_FRAMEBUFFER) {
+	else if (cmd == CMD_FRAMEBUFFER) {
 		return (uint64_t) get_framebuffer();
 	}
 
-	else if (command == CMD_FLIP) {
+	else if (cmd == CMD_FLIP) {
 		return flip();
 	}
 
-	else if (command == CMD_RESET) {
+	else if (cmd == CMD_RESET) {
 		return reset();
 	}
 
