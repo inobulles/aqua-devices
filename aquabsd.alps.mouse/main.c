70ca299d1a28d1e839adae392e139419d659305f
obiwac <obiwac@gmail.com>
2022-07-14 16:35:31 +0200

    mouse: Remove function arguments from `load`

--- aquabsd.alps.mouse/main.c
+++ aquabsd.alps.mouse/main.c
@@ -4,22 +4,20 @@
 #include <aquabsd.alps.mouse/private.h>
 #include <aquabsd.alps.mouse/functions.h>
 
-#define CMD_GET_DEFAULT_MOUSE_ID 0x646D // 'dm'
-#define CMD_UPDATE_MOUSE         0x756D // 'um'
-#define CMD_POLL_BUTTON          0x7062 // 'pb'
-#define CMD_POLL_AXIS            0x7061 // 'pa'
+typedef enum {
+	CMD_GET_DEFAULT_MOUSE_ID = 0x646D, // 'dm'
+	CMD_UPDATE_MOUSE         = 0x756D, // 'um'
+	CMD_POLL_BUTTON          = 0x7062, // 'pb'
+	CMD_POLL_AXIS            = 0x7061, // 'pa'
+} cmd_t;
 
-int load(
-	uint64_t (*kos_query_device) (uint64_t, uint64_t name),
-	void* (*kos_load_device_function) (uint64_t device, const char* name),
-	uint64_t (*kos_callback) (uint64_t callback, int argument_count, ...)) {
-
+int load(void) {
 	mouse_count = 0;
 	mice = NULL;
 
-	#if defined(AQUABSD_CONSOLE_MOUSE)
-		aquabsd_console_mouse_id = register_mouse("aquaBSD console mouse", update_aquabsd_console_mouse, NULL, 1)->id;
-	#endif
+#if defined(AQUABSD_CONSOLE_MOUSE)
+	aquabsd_console_mouse_id = register_mouse("aquaBSD console mouse", update_aquabsd_console_mouse, NULL, 1)->id;
+#endif
 
 	return 0;
 }
@@ -28,28 +26,29 @@ void quit(void) {
 	free(mice);
 }
 
-uint64_t send(uint16_t command, void* data) {
-	uint64_t* arguments = (uint64_t*) data;
+uint64_t send(cmd_t _cmd, void* data) {
+	cmd_t cmd = _cmd;
+	uint64_t* args = data;
 
-	if (command == CMD_GET_DEFAULT_MOUSE_ID) {
+	if (cmd == CMD_GET_DEFAULT_MOUSE_ID) {
 		return get_default_mouse_id();
 	}
 
-	else if (command == CMD_UPDATE_MOUSE) {
-		unsigned mouse_id = arguments[0];
+	else if (cmd == CMD_UPDATE_MOUSE) {
+		unsigned mouse_id = args[0];
 		return (uint64_t) update_mouse(mouse_id);
 	}
 
-	else if (command == CMD_POLL_BUTTON) {
-		unsigned mouse_id = arguments[0];
-		button_t button = arguments[1];
+	else if (cmd == CMD_POLL_BUTTON) {
+		unsigned mouse_id = args[0];
+		button_t button = args[1];
 
 		return (uint64_t) poll_button(mouse_id, button);
 	}
 
-	else if (command == CMD_POLL_AXIS) {
-		unsigned mouse_id = arguments[0];
-		axis_t axis = arguments[1];
+	else if (cmd == CMD_POLL_AXIS) {
+		unsigned mouse_id = args[0];
+		axis_t axis = args[1];
 
 		float result = poll_axis(mouse_id, axis);
 		return *(uint64_t*) &result;
