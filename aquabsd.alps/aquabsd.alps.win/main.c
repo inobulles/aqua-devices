4ab600247fc4c4a8c56364153afd593c798389ab
obiwac <obiwac@gmail.com>
2022-10-02 18:33:34 +0200

    aquabsd.alps.win: `CMD_GET_FB` command

--- aquabsd.alps/aquabsd.alps.win/main.c
+++ aquabsd.alps/aquabsd.alps.win/main.c
@@ -14,7 +14,7 @@ typedef enum {
 	CMD_LOOP                = 0x6C6F, // 'lo'
 
 	CMD_CLOSE               = 0x636C, // 'cl'
-	CMD_GRAB_FOCUS          = 0x6667, // 'gf'
+	CMD_GRAB_FOCUS          = 0x6766, // 'gf'
 
 	CMD_MODIFY              = 0x6D76, // 'mv'
 
@@ -35,6 +35,10 @@ typedef enum {
 
 	CMD_GET_DWD_CLOSE_POS_X = 0x3D78, // '=x'
 	CMD_GET_DWD_CLOSE_POS_Y = 0x3D79, // '=y'
+
+	// framebuffer stuff
+
+	CMD_GET_FB              = 0x6662, // 'fb'
 } cmd_t;
 
 int load(void) {
@@ -55,137 +59,146 @@ int load(void) {
 }
 
 uint64_t send(uint16_t _cmd, void* data) {
-	cmd_t cmd = _cmd;
-	uint64_t* args = data;
+	cmd_t const cmd = _cmd;
+	uint64_t* const args = data;
 
 	if (cmd == CMD_CREATE) {
-		unsigned x_res = args[0];
-		unsigned y_res = args[1];
+		unsigned const x_res = args[0];
+		unsigned const y_res = args[1];
 
 		return (uint64_t) create(x_res, y_res);
 	}
 
 	else if (cmd == CMD_DELETE) {
-		win_t* win = (void*) args[0];
+		win_t* const win = (void*) args[0];
 		return delete(win);
 	}
 
 	else if (cmd == CMD_SET_CAPTION) {
-		win_t* win = (void*) args[0];
-		const char* caption = (void*) args[1];
+		win_t* const win = (void*) args[0];
+		const char* const caption = (void*) args[1];
 
 		return set_caption(win, caption);
 	}
 
 	else if (cmd == CMD_GET_CAPTION) {
-		win_t* win = (void*) args[0];
+		win_t* const win = (void*) args[0];
 		return (uint64_t) get_caption(win);
 	}
 
 	else if (cmd == CMD_GET_STATE) {
-		win_t* win = (void*) args[0];
+		win_t* const win = (void*) args[0];
 		return get_state(win);
 	}
 
 	else if (cmd == CMD_REGISTER_CB) {
-		win_t* win = (void*) args[0];
-		cb_t type = args[1];
+		win_t* const win = (void*) args[0];
+		cb_t const type = args[1];
 
-		uint64_t cb = args[2];
-		uint64_t param = args[3];
+		uint64_t const cb = args[2];
+		uint64_t const param = args[3];
 
 		return register_cb(win, type, cb, param);
 	}
 
 	else if (cmd == CMD_LOOP) {
-		win_t* win = (void*) args[0];
+		win_t* const win = (void*) args[0];
 		return loop(win);
 	}
 
 	else if (cmd == CMD_CLOSE) {
-		win_t* win = (void*) args[0];
+		win_t* const win = (void*) args[0];
 		return close_win(win);
 	}
 
 	else if (cmd == CMD_GRAB_FOCUS) {
-		win_t* win = (void*) args[0];
+		win_t* const win = (void*) args[0];
 		return grab_focus(win);
 	}
 
 	else if (cmd == CMD_MODIFY) {
-		win_t* win = (void*) args[0];
+		win_t* const win = (void*) args[0];
 
-		float x = *(float*) &args[1];
-		float y = *(float*) &args[2];
+		float const x = *(float*) &args[1];
+		float const y = *(float*) &args[2];
 
-		unsigned x_res = args[3];
-		unsigned y_res = args[4];
+		unsigned const x_res = args[3];
+		unsigned const y_res = args[4];
 
 		return modify(win, x, y, x_res, y_res);
 	}
 
 	else if (cmd == CMD_GET_X_POS) {
-		win_t* win = (void*) args[0];
+		win_t* const win = (void*) args[0];
 
-		float x_pos = get_x_pos(win);
+		float const x_pos = get_x_pos(win);
 		return ((union { float _; uint64_t u; }) x_pos).u;
 	}
 
 	else if (cmd == CMD_GET_Y_POS) {
-		win_t* win = (void*) args[0];
+		win_t* const win = (void*) args[0];
 
-		float y_pos = get_y_pos(win);
+		float const y_pos = get_y_pos(win);
 		return ((union { float _; uint64_t u; }) y_pos).u;
 	}
 
 	else if (cmd == CMD_GET_X_RES) {
-		win_t* win = (void*) args[0];
+		win_t* const win = (void*) args[0];
 		return get_x_res(win);
 	}
 
 	else if (cmd == CMD_GET_Y_RES) {
-		win_t* win = (void*) args[0];
+		win_t* const win = (void*) args[0];
 		return get_y_res(win);
 	}
 
 	else if (cmd == CMD_GET_WM_X_RES) {
-		win_t* win = (void*) args[0];
+		win_t* const win = (void*) args[0];
 		return get_wm_x_res(win);
 	}
 
 	else if (cmd == CMD_GET_WM_Y_RES) {
-		win_t* win = (void*) args[0];
+		win_t* const win = (void*) args[0];
 		return get_wm_y_res(win);
 	}
 
 	// AQUA DWD protocol stuff
 
 	else if (cmd == CMD_SUPPORTS_DWD) {
-		win_t* win = (void*) args[0];
+		win_t* const win = (void*) args[0];
 		return supports_dwd(win);
 	}
 
 	else if (cmd == CMD_SET_DWD_CLOSE_POS) {
-		win_t* win = (void*) args[0];
+		win_t* const win = (void*) args[0];
 
-		float x = *(float*) &args[1];
-		float y = *(float*) &args[2];
+		float const x = *(float*) &args[1];
+		float const y = *(float*) &args[2];
 
 		return set_dwd_close_pos(win, x, y);
 	}
 
 	else if (cmd == CMD_GET_DWD_CLOSE_POS_X) {
-		win_t* win = (void*) args[0];
+		win_t* const win = (void*) args[0];
 
-		float x = get_dwd_close_pos_x(win);
+		float const x = get_dwd_close_pos_x(win);
 		return ((union { float f; uint64_t u; }) x).u;
 	}
 
 	else if (cmd == CMD_GET_DWD_CLOSE_POS_Y) {
-		win_t* win = (void*) args[0];
+		win_t* const win = (void*) args[0];
 
-		float y = get_dwd_close_pos_y(win);
+		float const y = get_dwd_close_pos_y(win);
 		return ((union { float f; uint64_t u; }) y).u;
+	}
+
+	// framebuffer stuff
+
+	else if (cmd == CMD_GET_FB) {
+		win_t* const win = (void*) args[0];
+		uint8_t const bpp = args[1];
+
+		return (uint64_t) get_fb(win, bpp);
 	}
 
 	return -1;
