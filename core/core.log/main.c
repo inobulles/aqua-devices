7bcdfabe39c2e8e7c200b39eae55f404cc7e6cfa
Obiwac <obiwac@gmail.com>
2022-10-07 12:33:29 +0200

    core.log: Go ham with them `const` qualifiers
    (To reflect the recent changes in Umber.)

--- core/core.log/main.c
+++ core/core.log/main.c
@@ -5,18 +5,18 @@ typedef enum {
 } cmd_t;
 
 uint64_t send(uint16_t _cmd, void* data) {
-	cmd_t cmd = _cmd;
+	cmd_t const cmd = _cmd;
 	uint64_t* args = data;
 
 	if (cmd == CMD_LOG) {
-		umber_lvl_t lvl = args[0];
+		umber_lvl_t const lvl = args[0];
 
-		const char* component = (void*) (intptr_t) args[1];
-		const char* path      = (void*) (intptr_t) args[2];
-		const char* func      = (void*) (intptr_t) args[3];
+		char const* const component = (void*) (intptr_t) args[1];
+		char const* const path      = (void*) (intptr_t) args[2];
+		char const* const func      = (void*) (intptr_t) args[3];
 
-		uint32_t line = args[4];
-		char* msg = (void*) (intptr_t) args[5];
+		uint32_t const line = args[4];
+		char const* const msg = (void*) (intptr_t) args[5];
 
 		umber_log(lvl, component, path, func, line, msg);
 		return 0;
