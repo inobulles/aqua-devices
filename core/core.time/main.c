84c6490bac86b41d691bb9ddd856d2fb2599a054
obiwac <obiwac@gmail.com>
2022-12-04 12:14:24 +0100

    core.time: Bob building setup (and code style improvements)

--- core/core.time/main.c
+++ core/core.time/main.c
@@ -1,10 +1,14 @@
 #include <time.h>
 #include <stdint.h>
 
-#define CMD_UNIX 0x7578 // 'ux'
+typedef enum {
+	CMD_UNIX = 0x7578, // 'ux'
+} cmd_t;
 
-uint64_t send(uint16_t command, void* data) {
-	if (command == CMD_UNIX) {
+uint64_t send(uint16_t _cmd, __attribute__((unused)) void* data) {
+	cmd_t cmd = _cmd;
+
+	if (cmd == CMD_UNIX) {
 		return time(NULL);
 	}
 
