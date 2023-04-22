9a32da5483315c59fa99c341352d8ffc6fc42ce9
obiwac <obiwac@gmail.com>
2023-04-22 18:02:29 +0200

    vk: Returning address of surface correctly now

--- aquabsd.alps/aquabsd.alps.vk/main.c
+++ aquabsd.alps/aquabsd.alps.vk/main.c
@@ -77,7 +77,7 @@ uint64_t send(uint16_t _cmd, void* data) {
 
 	else if (cmd == CMD_GET_SURFACE) {
 		context_t* context = (void*) args[0];
-		return (uint64_t) context->surface;
+		return (uint64_t) &context->surface;
 	}
 
 	return -1;
