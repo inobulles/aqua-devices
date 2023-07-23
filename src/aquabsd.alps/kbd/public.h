1aed978c861a8b5849c0d2188d032203ffc4c188
obiwac <obiwac@gmail.com>
2023-07-23 22:41:15 +0200

    kbd: Prevent duplicate symbols for function pointers

--- src/aquabsd.alps/kbd/public.h
+++ src/aquabsd.alps/kbd/public.h
@@ -1,5 +1,4 @@
-#if !defined(__AQUABSD_ALPS_KBD)
-#define __AQUABSD_ALPS_KBD
+#pragma once
 
 #include <stdbool.h>
 #include <stddef.h>
@@ -32,18 +31,20 @@ struct aquabsd_alps_kbd_t {
 	#define AQUABSD_ALPS_KBD_AQUABSD_CONSOLE_KBD
 #endif
 
-unsigned (*aquabsd_alps_kbd_get_default_kbd_id) (void);
-int (*aquabsd_alps_kbd_update_kbd) (unsigned kbd_id);
+#define FP static __attribute__((unused))
 
-unsigned (*aquabsd_alps_kbd_poll_button) (unsigned kbd_id, unsigned button);
+FP unsigned (*aquabsd_alps_kbd_get_default_kbd_id) (void);
+FP int (*aquabsd_alps_kbd_update_kbd) (unsigned kbd_id);
 
-unsigned (*aquabsd_alps_kbd_get_buf_len) (unsigned kbd_id);
-char* (*aquabsd_alps_kbd_get_buf) (unsigned kbd_id);
+FP unsigned (*aquabsd_alps_kbd_poll_button) (unsigned kbd_id, unsigned button);
 
-unsigned (*aquabsd_alps_kbd_get_keys_len) (unsigned kbd_id);
-const char** (*aquabsd_alps_kbd_get_keys) (unsigned kbd_id);
+FP unsigned (*aquabsd_alps_kbd_get_buf_len) (unsigned kbd_id);
+FP char* (*aquabsd_alps_kbd_get_buf) (unsigned kbd_id);
 
-aquabsd_alps_kbd_t* (*aquabsd_alps_kbd_register_kbd) (const char* name, aquabsd_alps_kbd_update_callback_t update_callback, void* update_cb_param, unsigned set_default);
-const char* (*aquabsd_alps_kbd_x11_map) (int key);
+FP unsigned (*aquabsd_alps_kbd_get_keys_len) (unsigned kbd_id);
+FP char const** (*aquabsd_alps_kbd_get_keys) (unsigned kbd_id);
 
-#endif
+FP aquabsd_alps_kbd_t* (*aquabsd_alps_kbd_register_kbd) (char const* name, aquabsd_alps_kbd_update_callback_t update_callback, void* update_cb_param, unsigned set_default);
+FP char const* (*aquabsd_alps_kbd_x11_map) (int key);
+
+#undef FP
